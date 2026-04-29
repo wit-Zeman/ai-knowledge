9fd9ad6cd48dfa8923dae1674632d09a4d46b026
zhangzhen <404notfoundya@gmail.com>
2026-04-29 10:36:36 +0800

    init

---
+++ src/main/java/com/emo/fish/ai/AiKnowledgeApplication.java
@@ -0,0 +1,13 @@
+package com.emo.fish.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiKnowledgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiKnowledgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emo/fish/ai/AiKnowledgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emo.fish.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiKnowledgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
